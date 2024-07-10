22ea94f373df682d465dbb239a10b39054b4776e
Alvirah-Ro <bajoraneh@hotmail.com>
2024-07-10 17:40:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import EnterWords from './EnterWords.js'
 
 function App() {
   return (
+    <div>
+      <EnterWords />
+    </div>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
