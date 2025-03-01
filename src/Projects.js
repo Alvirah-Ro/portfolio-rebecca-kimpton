1d477b4967c600d1bc1633d7b77454c0f6eaf7fa
Alvirah-Ro <bajoraneh@hotmail.com>
2025-03-01 12:59:29 -0600

    added Polls App to project

--- src/Projects.js
+++ src/Projects.js
@@ -1,15 +1,21 @@
 import Schuyler from './Schuyler.jpeg';
+import Polls_HomePage from './Polls_HomePage.jpeg';
+import PollsHomePage from './PollsHomePage.jpg';
 
 export default function Projects() {
     return (
         <div className="container">
             {/* <div className="grid gap-3"> */}
                 <div className="row column-gap-3 mt-5">
-                    <div className="col p-3" style={{ backgroundColor: '#fc8eac' }}>
-                        <img className="img-fluid" src={Schuyler} alt="project photo" />
-                        <h4>Project Name</h4>
-                        <p>Project brief description</p>
+                    <div className="col p-3">
+                        <div className="border border-dark text-center p-3">
+                            <h4>Polls App</h4>
+                            <p>Polls is a Django app that allows users to view and vote on various fun questions.</p>
+                            <img className="object-fit-contain mt-n5" src={PollsHomePage} alt="Home Page of Polls App" />
+                        </div>
                     </div>
+                </div>
+                <div className="row column-gap-3 mt-5">
                     <div className="col p-3" style={{ backgroundColor: '#fc6c85' }}>
                         <img className="img-fluid" src={Schuyler} alt="project photo" />
                         <h4>Project Name</h4>
@@ -31,6 +37,5 @@ export default function Projects() {
             </div>    
             <h5>Git Hub link:</h5><a href="https://github.com/Alvirah-Ro">https://github.com/Alvirah-Ro</a>
         </div>
-
-            )
+    )
 }
