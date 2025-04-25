1b90326c75a9626389a05f5921d23baca552bee5
Alvirah-Ro <bajoraneh@hotmail.com>
2025-04-25 16:30:35 -0500

    fixed overlapping problem on footer

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,10 @@
 export default function Contact() {
     return (
         <footer className="bg-dark text-white py-3 mt-5" data-bs-theme="dark">
-            <div className="row justify-content-start px-5">
-
-            <div className="col-2 text-center">BajoranEH@hotmail.com</div>
-                <a href="https://www.linkedin.com/in/rebecca-kimpton-18a047154/" className="col-2 white text-center">LinkedIn Profile</a>
-                <a href="https://github.com/Alvirah-Ro?tab=repositories" className="col-2 white">GitHub Profile</a>
+            <div className="row flex-wrap justify-content-start px-5">
+                <div className="col-6">BajoranEH@hotmail.com</div>
+                <a href="https://www.linkedin.com/in/rebecca-kimpton-18a047154/" className="col-4 white">LinkedIn Profile</a>
+                <a href="https://github.com/Alvirah-Ro?tab=repositories" className="col-4 white">GitHub Profile</a>
             </div>
         </footer>
     )
