c3fbf7f3af68d4ebebfa1c8370897549bd2fe4ad
Alvirah-Ro <bajoraneh@hotmail.com>
2025-05-05 15:58:04 -0500

    modified summary

--- src/Summary.js
+++ src/Summary.js
@@ -5,10 +5,12 @@ export default function Summary() {
             <div className="mx-4 p-4 text-center lavender rebeccaBackground">
                 <div className="mx-4">
                 I am a dedicated professional with over 15 years of experience in customer service, now transitioning into a career in software and application development. 
-                A quick and enthusiastic learner, I have pursued language studies in German, Russian, Swedish, and French, and I bring the same passion for learning to developing my technical skills. 
-                I am eager to continue expanding my programming knowledge, grow on the job, and contribute to a collaborative team environment. 
-                I am a strong problem solver with excellent communication skills, and I bring a positive attitude and a strong work ethic to help any company succeed.    
-            </div>
+                I recently completed the IBM Full Stack Software Developer Professional Certificate and am a quick, enthusiastic learner. 
+                In my free time, I enjoy exploring new languages — including German, Russian, Swedish, and currently French - simply for the challenge and joy of learning. 
+                I bring this same curiosity and passion to developing my technical skills. 
+                I’m eager to continue growing on the job, contribute to a collaborative team, and apply my strong problem-solving and communication abilities. 
+                With a positive attitude and strong work ethic, I strive to support company success in any role.
+                </div>
             </div>
         </div>
     )
