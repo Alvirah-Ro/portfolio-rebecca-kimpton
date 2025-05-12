e6cf858cca8cc6eba538c1fabcd95f8d1142424d
Alvirah-Ro <bajoraneh@hotmail.com>
2025-05-12 16:03:02 -0500

    changed github link

--- src/Contact.js
+++ src/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
     return (
         <footer className="bg-dark text-white py-3 mt-5" data-bs-theme="dark">
             <div className="row flex-wrap justify-content-start px-5">
-                <a href="https://github.com/Alvirah-Ro?tab=repositories" className="col-md-2 white">GitHub</a>
+                <a href="https://github.com/Alvirah-Ro" className="col-md-2 white">GitHub</a>
                 <a href="https://www.linkedin.com/in/rebecca-kimpton-18a047154/" className="col-md-2 white">LinkedIn</a>
                 <a href={`mailto:${email}`} className="col-md-4 white">{email}</a>
             </div>
