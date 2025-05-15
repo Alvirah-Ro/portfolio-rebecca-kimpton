c15c895ca5daafeeb535c2b3c93effd320931947
Alvirah-Ro <bajoraneh@hotmail.com>
2025-05-15 14:17:27 -0500

    arranging order of components on resume page

--- RebeccaKimptonPortfolio/src/Resume.js
+++ RebeccaKimptonPortfolio/src/Resume.js
@@ -1,8 +1,8 @@
+import Summary from './Summary';
 import TechnicalSkills from './TechnicalSkills';
 import Certifications from './Certifications';
 import WorkExperience from './WorkExperience';
 import Education from './Education';
-import Summary from './Summary';
 
 export default function Resume() {
     return (
