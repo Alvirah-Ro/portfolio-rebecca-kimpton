c51620097d0d8ed75290efe5c73e23d90046140d
Alvirah-Ro <bajoraneh@hotmail.com>
2025-08-14 10:26:40 -0500

    added info for learning about software testing to current learning section

--- src/Current.js
+++ src/Current.js
@@ -4,6 +4,10 @@ export default function Current() {
             <h1 className="m-3 mt-5 p-3 text-center rebecca">CURRENT LEARNING AND PROJECTS</h1>
             <div className="mx-4 p-4 lavender rebeccaBackground">
                 <div className="mx-4">
+                    <h4 className="py-2">Software Testing</h4>
+                    <ul>
+                        <li>I am currently reading the ISTQB Certified Tester Foundation Level Syllabus to learn more about software testing.  Upon completion, I am planning on practicing writing my own test plans, test cases, and bug reports.</li>
+                    </ul>
                     <h4 className="py-2">Microsoft Learn Learning Path: Get started with C#</h4>
                     <ul>
                         <li>I recently completed Part 1 of this Learning Path: Write your first code using C#.  Next step is Part 2: Create and Run Simple C# Console Applications</li>
