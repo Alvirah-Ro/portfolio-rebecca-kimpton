ef83c06ced29a4be8608baab3f9dabba9003d7bf
Alvirah-Ro <bajoraneh@hotmail.com>
2025-08-17 17:01:40 -0500

    added SQL to current learning

---
+++ src/Blog.js
@@ -0,0 +1,11 @@
+// This file contains former entries that could be turned into a blog
+export default function Blog() {
+    return (
+        <div className="container">
+            <h4 className="py-2">Microsoft Learn Learning Path: Get started with C#</h4>
+            <ul>
+                <li>I recently completed Part 1 of this Learning Path: Write your first code using C# Next step is Part 2: Create and Run Simple C# Console Applications</li>
+            </ul>
+        </div>
+    )
+}
--- src/Current.js
+++ src/Current.js
@@ -6,15 +6,15 @@ export default function Current() {
                 <div className="mx-4">
                     <h4 className="py-2">Software Testing</h4>
                     <ul>
-                        <li>I am currently reading the ISTQB Certified Tester Foundation Level Syllabus to learn more about software testing.  Upon completion, I am planning on practicing writing my own test plans, test cases, and bug reports.</li>
+                        <li>August 14th: I am currently reading the ISTQB Certified Tester Foundation Level Syllabus to learn more about software testing.  Upon completion, I am planning on practicing writing my own test plans, test cases, and bug reports.</li>
                     </ul>
-                    <h4 className="py-2">Microsoft Learn Learning Path: Get started with C#</h4>
+                    <h4 className="py-2">SQL</h4>
                     <ul>
-                        <li>I recently completed Part 1 of this Learning Path: Write your first code using C#.  Next step is Part 2: Create and Run Simple C# Console Applications</li>
+                        <li>August 17th: I just downloaded PostgreSQL and the Northwind Database.  I am using them to practice commands taught in the W3Schools tutorial.</li>
                     </ul>
                     <h4 className="py-2">Express Book Reviews App</h4>
                     <ul>
-                        <li>I'm currently working on updating a former course project, an Express app using Node.js.  I am incorporating MongoDB as the database and adding extra functionality.  I have updated the login/logout logic and public user routes.  I am currently working on the routes for registered users which includes adding, updating, and deleting book reviews.  View on <a className= "white" href= "https://github.com/Alvirah-Ro/expressBookReviews">GitHub.</a></li>
+                        <li>August 3rd: I'm currently working on updating a former course project, an Express app using Node.js.  I am incorporating MongoDB as the database and adding extra functionality.  I have updated the login/logout logic and public user routes.  I am currently working on the routes for registered users which includes adding, updating, and deleting book reviews.  View on <a className= "white" href= "https://github.com/Alvirah-Ro/expressBookReviews">GitHub.</a></li>
                     </ul>
                 </div>
             </div>
