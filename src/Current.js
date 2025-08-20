b6c2b6d02bc482eb4f00a072b95bd1ffde218d8f
Alvirah-Ro <bajoraneh@hotmail.com>
2025-08-20 18:52:44 -0500

    changed order of current projects

--- src/Current.js
+++ src/Current.js
@@ -4,13 +4,13 @@ export default function Current() {
             <h1 className="m-3 mt-5 p-3 text-center rebecca">CURRENT LEARNING AND PROJECTS</h1>
             <div className="mx-4 p-4 lavender rebeccaBackground">
                 <div className="mx-4">
-                    <h4 className="py-2">Software Testing</h4>
-                    <ul>
-                        <li>August 14th: I am currently reading the ISTQB Certified Tester Foundation Level Syllabus to learn more about software testing.  Upon completion, I am planning on practicing writing my own test plans, test cases, and bug reports.</li>
-                    </ul>
                     <h4 className="py-2">SQL</h4>
                     <ul>
                         <li>August 17th: I just downloaded PostgreSQL and the Northwind Database.  I am using them to practice commands taught in the W3Schools tutorial.</li>
+                    </ul>
+                    <h4 className="py-2">Software Testing</h4>
+                    <ul>
+                        <li>August 14th: I am currently reading the ISTQB Certified Tester Foundation Level Syllabus to learn more about software testing.  Upon completion, I am planning on practicing writing my own test plans, test cases, and bug reports.</li>
                     </ul>
                     <h4 className="py-2">Express Book Reviews App</h4>
                     <ul>
