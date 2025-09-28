a4be1c788adad77b8a881eb6d71dc11aaaff556c
Alvirah-Ro <bajoraneh@hotmail.com>
2025-09-28 14:54:29 -0500

    wrote about the new conversion page in Current

--- src/Blog.js
+++ src/Blog.js
@@ -2,9 +2,20 @@
 export default function Blog() {
     return (
         <div className="container">
+            <h4 className="py-2">Angular</h4>
+            <ul>
+                <li>August 29th: I'm currently building an Angular application, an app to find rental homes, through the official tutorial in my local development environment.  </li>
+            </ul>
+
+            <h4 className="py-2">Software Testing</h4>
+            <ul>
+                <li>August 14th: I am currently reading the ISTQB Certified Tester Foundation Level Syllabus to learn more about software testing.  Upon completion, I am planning on practicing writing my own test plans, test cases, and bug reports.</li>
+            </ul>
+
+
             <h4 className="py-2">Express Book Reviews App</h4>
             <ul>
-                <li>August 3rd: I'm currently working on updating a former course project, an Express app using Node.js.  I am incorporating MongoDB as the database and adding extra functionality.  I have updated the login/logout logic and public user routes.  I am currently working on the routes for registered users which includes adding, updating, and deleting book reviews.  View on <a className= "white" href= "https://github.com/Alvirah-Ro/expressBookReviews">GitHub.</a></li>
+                <li>August 3rd: I'm currently working on updating a former course project, an Express app using Node.js.  I am incorporating MongoDB as the database and adding extra functionality.  I have updated the login/logout logic and public user routes.  I am currently working on the routes for registered users which includes adding, updating, and deleting book reviews.  View on <a className="white" href="https://github.com/Alvirah-Ro/expressBookReviews">GitHub.</a></li>
             </ul>
 
             <h4 className="py-2">Microsoft Learn Learning Path: Get started with C#</h4>
--- src/Current.js
+++ src/Current.js
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+
+
 export default function Current() {
     return (
         <div className="container">
@@ -5,27 +8,27 @@ export default function Current() {
             <div className="mx-4 p-4 lavender rebeccaBackground">
                 <div className="mx-4">
 
+                    <h4 className="py-2">Conversion Page</h4>
+                    <ul>
+                        <li>September 28th: I've created a conversion page on this site to help me streamline calculations when reviewing invoices at work.</li>
+                            Check out the top nav link or view the page <Link to="/conversion" className="white">here</Link>.
+                    </ul>
+
                     <h4 className="py-2">More Typescript</h4>
                     <ul>
                         <li>September 17th: After completing the Angular tutorial, I am continuing some practice with typescript using an array of mock patient data. </li>
-                            View on <a href="https://github.com/Alvirah-Ro/PatientsApp" className="white">GitHub</a>
+                            View on <a href="https://github.com/Alvirah-Ro/PatientsApp" className="white">GitHub</a>.
                     </ul>
 
                     <h4 className="py-2">Angular</h4>
                     <ul>
-                        <li>September 26th: I have completed the official tutorial and am now editing and enhancing the app on my own.  Today I worked on adding validations and a success message to the form submission in the details component. </li>
-                        <li>August 29th: I'm currently building an Angular application, an app to find rental homes, through the official tutorial in my local development environment.  </li>
-                            View on <a href="https://github.com/Alvirah-Ro/HomesApp" className="white">GitHub</a>
+                        <li>September 26th: I have completed the official Angular app tutorial and am now editing and enhancing the homes app on my own.  Today I worked on adding validations and a success message to the form submission in the details component. </li>
+                            View on <a href="https://github.com/Alvirah-Ro/HomesApp" className="white">GitHub</a>.
                     </ul>
 
                     <h4 className="py-2">SQL</h4>
                     <ul>
                         <li>August 17th: I just downloaded PostgreSQL and the Northwind Database.  I am using them to practice commands taught in the W3Schools tutorial.</li>
-                    </ul>
-                    
-                    <h4 className="py-2">Software Testing</h4>
-                    <ul>
-                        <li>August 14th: I am currently reading the ISTQB Certified Tester Foundation Level Syllabus to learn more about software testing.  Upon completion, I am planning on practicing writing my own test plans, test cases, and bug reports.</li>
                     </ul>
                 </div>
             </div>
